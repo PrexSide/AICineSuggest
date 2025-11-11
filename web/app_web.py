aa87df3d2d01728448afd49142dfbfce51a6a57b
Pratik Ramagare <pratikramagare12005@gmail.com>
2025-11-11 15:51:06 +0530

    Prepare Flask app for Render deployment

--- web/app_web.py
+++ web/app_web.py
@@ -294,6 +294,6 @@ def api_autocomplete():
 
 if __name__ == "__main__":
 	with app.app_context():
-		db.create_all()
-	port = int(os.environ.get("PORT", 8000))
+		db.create_all() 
+	port = int(os.environ.get("PORT", 5000))  
 	app.run(host="0.0.0.0", port=port, debug=False)
